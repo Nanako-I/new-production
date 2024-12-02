17e90b8dbed42b33e4a20a5f01e9bb331c522a70
Nanako-I <milklmi.1103775@gmail.com>
2024-12-02 22:52:51 +0900

    チャットの機能追加

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -379,74 +379,74 @@
                 chatField.scrollTop = chatField.scrollHeight;
             }
 
-            document.addEventListener('DOMContentLoaded', (event) => {
-                chatToBottom();
-                const userIdentifier = document.getElementById('chatbot-text').getAttribute('data-user-identifier');
+            // document.addEventListener('DOMContentLoaded', (event) => {
+            //     chatToBottom();
+            //     const userIdentifier = document.getElementById('chatbot-text').getAttribute('data-user-identifier');
 
-                window.displayMessage = function(data) {
-                    const chatUl = document.getElementById('chatbot-ul');
-                    if (!document.querySelector(`li[data-message-id="${data.id}"]`)) {
-                        const li = document.createElement('li');
-                        const className = data.user_identifier == window.sessionUserIdentifier ? 'self' : 'other';
-                        li.classList.add(className);
-                        li.setAttribute('data-message-id', data.id);
+            //     window.displayMessage = function(data) {
+            //         const chatUl = document.getElementById('chatbot-ul');
+            //         if (!document.querySelector(`li[data-message-id="${data.id}"]`)) {
+            //             const li = document.createElement('li');
+            //             const className = data.user_identifier == window.sessionUserIdentifier ? 'self' : 'other';
+            //             li.classList.add(className);
+            //             li.setAttribute('data-message-id', data.id);
 
-                        li.innerHTML = `
-                            <div class="message-container ${className === 'self' ? 'self-message' : 'other-message'}">
-                                <div style="overflow-wrap: break-word;">
-                                    <p style="overflow-wrap: break-word;" class="text-gray-900">${data.message}</p>
-                                    ${data.filename ? `<img alt="team" class="w-80 h-64" src="/storage/sample/chat_photo/${data.filename}" onerror="this.onerror=null;">` : ''}
-                                </div>
-                                <p class="text-sm font-normal ${className === 'self' ? 'text-right' : 'text-left'}">
-                                    ${data.created_at} ＠${data.last_name}${data.first_name}
-                                </p>
-                            </div>
-                        `;
-                        chatUl.appendChild(li);
-                        chatToBottom();
-                    }
-                };
+            //             li.innerHTML = `
+            //                 <div class="message-container ${className === 'self' ? 'self-message' : 'other-message'}">
+            //                     <div style="overflow-wrap: break-word;">
+            //                         <p style="overflow-wrap: break-word;" class="text-gray-900">${data.message}</p>
+            //                         ${data.filename ? `<img alt="team" class="w-80 h-64" src="/storage/sample/chat_photo/${data.filename}" onerror="this.onerror=null;">` : ''}
+            //                     </div>
+            //                     <p class="text-sm font-normal ${className === 'self' ? 'text-right' : 'text-left'}">
+            //                         ${data.created_at} ＠${data.last_name}${data.first_name}
+            //                     </p>
+            //                 </div>
+            //             `;
+            //             chatUl.appendChild(li);
+            //             chatToBottom();
+            //         }
+            //     };
 
-                const chatForm = document.getElementById('chat-form');
-                if (chatForm) {
-                    chatForm.addEventListener('submit', function(e) {
-                        e.preventDefault();
-                        var formData = new FormData(chatForm);
+            //     const chatForm = document.getElementById('chat-form');
+            //     if (chatForm) {
+            //         chatForm.addEventListener('submit', function(e) {
+            //             e.preventDefault();
+            //             var formData = new FormData(chatForm);
 
-                        const isFileSelected = document.getElementById('filename').files.length > 0;
+            //             const isFileSelected = document.getElementById('filename').files.length > 0;
 
-                        if (isFileSelected) {
-                            formData.append('message', '写真が送信されました');
-                        }
+            //             if (isFileSelected) {
+            //                 formData.append('message', '写真が送信されました');
+            //             }
 
-                        fetch(this.action, {
-                            method: 'POST',
-                            body: formData
-                        })
-                        .then(response => {
-                            if (!response.ok) {
-                                throw new Error('Network response was not ok');
-                            }
-                            return response.json();
-                        })
-                        .then(data => {
-                            console.log('Data:', data);
-                            if (data.error) {
-                                throw new Error(data.error);
-                            }
-                            // if (!document.querySelector(`li[data-message-id="${data.id}"]`)) {
-                            //     window.displayMessage(data);
-                            // }
-                            document.getElementById('chatbot-text').value = '';
-                            document.getElementById('filename').value = '';
-                        })
-                        .catch(error => {
-                            console.error('Error:', error);
-                            alert('メッセージの送信に失敗しました。');
-                        });
-                    }, { once: true });
-                }
-            });
+            //             fetch(this.action, {
+            //                 method: 'POST',
+            //                 body: formData
+            //             })
+            //             .then(response => {
+            //                 if (!response.ok) {
+            //                     throw new Error('Network response was not ok');
+            //                 }
+            //                 return response.json();
+            //             })
+            //             .then(data => {
+            //                 console.log('Data:', data);
+            //                 if (data.error) {
+            //                     throw new Error(data.error);
+            //                 }
+            //                 // if (!document.querySelector(`li[data-message-id="${data.id}"]`)) {
+            //                 //     window.displayMessage(data);
+            //                 // }
+            //                 document.getElementById('chatbot-text').value = '';
+            //                 document.getElementById('filename').value = '';
+            //             })
+            //             .catch(error => {
+            //                 console.error('Error:', error);
+            //                 alert('メッセージの送信に失敗しました。');
+            //             });
+            //         }, { once: true });
+            //     }
+            // });
         </script>
     <!-- </div> -->
 
